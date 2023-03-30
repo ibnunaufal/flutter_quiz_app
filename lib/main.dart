3cefd0567b94cef3f93bdb86e1a1d607bf0aa578
ibnu naufal <ibnunaufal5758@gmail.com>
2023-03-30 08:11:45 +0700

    implement 'if' statement

--- lib/main.dart
+++ lib/main.dart
@@ -14,39 +14,42 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var questionsIndex = 0;
+  var questions = [
+    {
+      'questionText': 'Apa warna favorit?',
+      'answer': ['Black', 'Blue', 'Red']
+    },
+    {
+      'questionText': 'Hewan favorit?',
+      'answer': ['Kucing', 'Anjing', 'Ikan']
+    }
+  ];
   void answerQuestion() {
-    setState(() {
-      questionsIndex += 1;
-    });
-    print("pencet");
+    if (questionsIndex < questions.length) {
+      setState(() {
+        questionsIndex += 1;
+      });
+      print("pencet");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'Apa warna favorit?',
-        'answer': ['Black', 'Blue', 'Red']
-      },
-      {
-        'questionText': 'Hewan favorit?',
-        'answer': ['Kucing', 'Anjing', 'Ikan']
-      }
-    ];
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Title")),
-        body: Column(children: [
-          Question(questions[questionsIndex]['questionText']),
-          ...(questions[questionsIndex]['answer'] as List<String>).map((ans) {
-            return Answer(answerQuestion, ans);
-          }).toList(),
-          ElevatedButton(onPressed: answerQuestion, child: Text('A')),
-          ElevatedButton(onPressed: answerQuestion, child: Text('B')),
-          ElevatedButton(onPressed: answerQuestion, child: Text('C')),
-          ElevatedButton(onPressed: answerQuestion, child: Text('D')),
-        ]),
+        body: questionsIndex < questions.length
+            ? Column(children: [
+                Question(questions[questionsIndex]['questionText']),
+                ...(questions[questionsIndex]['answer'] as List<String>)
+                    .map((ans) {
+                  return Answer(answerQuestion, ans);
+                }).toList(),
+              ])
+            : Center(
+                child: Text('Sudah selesai'),
+              ),
       ),
     );
   }
